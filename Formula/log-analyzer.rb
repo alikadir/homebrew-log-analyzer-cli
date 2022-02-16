0b06fc39ba63810c40213c765bb29339b15d521c
Ali K. Bagcioglu <alikadirbagcioglu@gmail.com>
2022-02-16 21:46:51 +0000

    update version v1.0.15

--- Formula/log-analyzer.rb
+++ Formula/log-analyzer.rb
@@ -7,8 +7,8 @@ require "language/node"
 class LogAnalyzer < Formula
    desc " ⏲ Log analyzer CLI for Heroku, AWS, etc."
    homepage "https://github.com/alikadir/log-analyzer-cli#readme"
-   url "https://registry.npmjs.org/log-analyzer-cli/-/log-analyzer-cli-1.0.14.tgz"
-   sha256 "4e8de2fc7174fda0daaadc115c3182e2d7c601d4b6f53ecbaaa2cbdc02d3cbbb"
+   url "https://registry.npmjs.org/log-analyzer-cli/-/log-analyzer-cli-1.0.15.tgz"
+   sha256 "1922f9118917fb61733a6da366d8933f9a90d58d6a9fef0d22d868b18c500c47"
 
    depends_on "node"
    def install
